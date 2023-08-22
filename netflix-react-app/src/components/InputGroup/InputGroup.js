f3530ac74df4fee752e2a3a51e55de70f8bed750
ruta-is-coding <rutajurg.123@gmail.com>
2023-08-22 12:04:23 +0300

    edited

--- netflix-react-app/src/components/InputGroup/InputGroup.js
+++ netflix-react-app/src/components/InputGroup/InputGroup.js
@@ -4,13 +4,21 @@ import Button from "../button/Button";
 const InputGroup = (props) => {
   return (
     <div className="row justify-content-center">
-      <div className="input-group w-50">
+      <div className="d-flex gap-2 w-50">
         <input
           type="text"
           className="form-control py-3"
           placeholder={props.placeholder}
         />
-        <Button title="Get Started" />
+        <Button
+          title="Get Started"
+          style={{
+            width: "200px",
+            backgroundColor: "red",
+            color: "white",
+            fontSize: "18px",
+          }}
+        />
       </div>
     </div>
   );
